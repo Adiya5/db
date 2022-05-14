f6672c1f773a3c3bd138907e937f9a09f6f12847
Adiya <adiya.sakhipkereyeva@gmail.com>
2022-05-14 07:47:23 +0600

    deploy heroku

--- controllers/User.js
+++ controllers/User.js
@@ -39,26 +39,33 @@ exports.findOne = async (req, res) => {
     }
 };
 exports.update = async (req, res) => {
-    if(!req.body) {
-        res.status(400).send({
-            message: "Data to update can not be empty!"
-        });
+
+    if (!req.body.newEmail || !req.body.newUsername || !req.body.newPassword) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        //res.status(400).render('results', {mydata: "Data to update can not be empty!"})
+        return
     }
 
-    const email = req.query.email;
+    //const email = req.params.oldEmail;
+    const email = req.body.oldEmail;
 
-    await UserModel.findOne(email, req.body, { useFindAndModify: false }).then(data => {
+    //await UserModel.findByIdAndUpdate(id, req.body, { useFindAndModify: false }).then(data => {
+    await UserModel.findOneAndUpdate({email: email}, {
+        email:req.body.newEmail,
+        username:req.body.newUsername,
+        password:req.body.newPassword
+    }).then(data => {
+        console.log(data)
         if (!data) {
-            res.status(404).send({
-                message: `User not found.`
-            });
+            res.status(404).send({message: `User not found.`});
+            //res.status(404).render('results', {mydata: `User not found.`})
         }else{
             res.send({ message: "User updated successfully." })
+            //res.status(200).render('results', {mydata: "User updated successfully."})
         }
     }).catch(err => {
-        res.status(500).send({
-            message: err.message
-        });
+        res.status(500).send({message: err.message});
+        //res.status(500).render('results', {mydata: err.message})
     });
 };
 exports.delete = async (req, res) => {
--- routes/User.js
+++ routes/User.js
@@ -2,8 +2,8 @@ const express = require('express')
 const UserController = require('../controllers/User')
 const router = express.Router();
 router.get('/', UserController.findAll);
-router.get('/:id', UserController.findOne);
+router.get('/:email', UserController.findOne);
 router.post('/', UserController.create);
-router.patch('/:id', UserController.update);
-router.delete('/:id', UserController.destroy);
+router.post('/:email', UserController.update);
+router.delete('/:email', UserController.delete);
 module.exports = router
